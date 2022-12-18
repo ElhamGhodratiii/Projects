3db5d9466203a858a560fd8d3febd11cc6af9da4
Elli <lham.ghodrati@gmail.com>
2022-12-18 18:23:34 +0100

    update browser-router

--- ticket-task/src/App.js
+++ ticket-task/src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import { QueryParamProvider } from 'use-query-params';
 import HomePage from './pages/HomePage';
 import TicketPage from './pages/TicketPage';
 import AllTicketsPage from './pages/AllTicketsPage';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom';
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <QueryParamProvider ReactRouterRoute={Route}>
-        <Switch>
+        <Routes>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/create-ticket" component={TicketPage} />
           <Route exact path="/all-tickets" component={AllTicketsPage} />
-        </Switch>
+        </Routes>
       </QueryParamProvider>
-    </Router>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
